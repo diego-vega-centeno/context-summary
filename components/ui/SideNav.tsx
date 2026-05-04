2ed28812263a9b3c63f43c408a16b96565b956c2
diego vega <da-vce@hotmail.com>
2026-05-03 23:05:14 -0500

    feat: create ThemeToggle component

--- components/ui/SideNav.tsx
+++ components/ui/SideNav.tsx
@@ -1,32 +1,15 @@
 "use client";
 import NavLinks from "./NavLinks";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { ThemeToggle } from "./ThemeToggle";
 
 export default function SideNav() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
   return (
     <div className="p-4 h-full flex flex-col border-r-1 border-r-red-background">
       <div className="h-11/12">
         <NavLinks />
       </div>
       <div className="h-1/12">
-        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-          {theme === "dark" ? (
-            <Sun className="h-8 w-8 p-1 rounded-full hover:cursor-pointer hover:bg-foreground hover:text-background" />
-          ) : (
-            <Moon className="h-8 w-8 p-1 rounded-full hover:cursor-pointer hover:bg-foreground hover:text-background" />
-          )}
-        </button>
+          <ThemeToggle/>
       </div>
     </div>
   );
---
+++ components/ui/ThemeToggle.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      {theme === "dark" ? (
+        <Sun className="h-8 w-8 p-1 rounded-full hover:bg-foreground hover:text-background" />
+      ) : (
+        <Moon className="h-8 w-8 p-1 rounded-full hover:bg-foreground hover:text-background" />
+      )}
+    </button>
+  );
+}
