d7a14b04edf60cc196e0dc0cb1d0fd0f6044da67
diego vega <da-vce@hotmail.com>
2026-05-03 23:02:19 -0500

    feat: use theme variables
    - add right border to SideNav

--- components/ui/SideNav.tsx
+++ components/ui/SideNav.tsx
@@ -15,16 +15,16 @@ export default function SideNav() {
   if (!mounted) return null;
 
   return (
-    <div className="p-4 h-full flex flex-col">
+    <div className="p-4 h-full flex flex-col border-r-1 border-r-red-background">
       <div className="h-11/12">
         <NavLinks />
       </div>
       <div className="h-1/12">
         <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
           {theme === "dark" ? (
-            <Sun className="h-8 w-8 p-1 rounded-full hover:cursor-pointer hover:bg-[var(--foreground)] hover:text-[var(--background)]" />
+            <Sun className="h-8 w-8 p-1 rounded-full hover:cursor-pointer hover:bg-foreground hover:text-background" />
           ) : (
-            <Moon className="h-8 w-8 p-1 rounded-full hover:cursor-pointer hover:bg-[var(--foreground)] hover:text-[var(--background)]" />
+            <Moon className="h-8 w-8 p-1 rounded-full hover:cursor-pointer hover:bg-foreground hover:text-background" />
           )}
         </button>
       </div>
