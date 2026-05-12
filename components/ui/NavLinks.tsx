e9b96c067544a69d4d6a00adcf1efdc95be4adc5
diego vega <da-vce@hotmail.com>
2026-05-12 12:49:18 -0500

    feat: remove unused imports and 'use client'

--- components/ui/NavLinks.tsx
+++ components/ui/NavLinks.tsx
@@ -1,7 +1,3 @@
-"use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
 import Button from "@/components/ui/Button";
 import { LayoutDashboard, BookOpenText } from "lucide-react";
 
