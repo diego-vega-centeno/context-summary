8967c8b41e327228ab719fe1dd7c82c1138eceb4
diego vega <da-vce@hotmail.com>
2026-05-18 17:05:41 -0500

    feat: create skeletons file

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -3,16 +3,17 @@ import PRMiniCard from "@/components/ui/dashboard/PRMiniCard";
 import DashboardCount from "@/components/ui/dashboard/DashboardCount";
 import PRMiniCardSkeleton from "@/components/ui/dashboard/PRMiniCardSkeleton";
 import DashboardStatus from "@/components/ui/dashboard/DashboardStatus";
-import DashboardStatusSkeleton from "@/components/ui/dashboard/DashboardStatusSkeleton";
+import { DashboardStatusSkeleton } from "@/components/ui/skeletons";
 
 export default async function DashboardPage() {
   return (
     <main className="flex flex-1 flex-col justify-between p-6 max-w-6xl mx-auto">
       <div>
         <DashboardCount />
-        <Suspense fallback={<DashboardStatusSkeleton />}>
+        {/* <Suspense fallback={<DashboardStatusSkeleton />}>
           <DashboardStatus />
-        </Suspense>
+        </Suspense> */}
+        <DashboardStatusSkeleton />
       </div>
     </main>
   );
