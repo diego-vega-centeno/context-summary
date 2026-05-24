76770d573f7c988e7711b055d884fe76580bfc80
diego vega <da-vce@hotmail.com>
2026-05-23 19:50:38 -0500

    feat: remove key lol

--- lib/data/services/gemini.ts
+++ lib/data/services/gemini.ts
@@ -1,25 +1,28 @@
 import { GoogleGenAI } from "@google/genai";
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-import { PRTimeline, SummaryJSON } from "@/types";
+import { TrackedPRWithTimeline, SummaryJSON } from "@/types";
 import { NARRATIVE_RECOVERY_PROMPT } from "../prompts";
-import logger from "@/lib/logger";
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
-export async function generatePRSummary(
-  timeline: PRTimeline,
+export async function makePRSummary(
+  prWithEvents: TrackedPRWithTimeline,
 ): Promise<SummaryJSON> {
-  const prompt = `${NARRATIVE_RECOVERY_PROMPT}\n\nINPUT DATA:\n${JSON.stringify(timeline)}`;
+  const prompt = `${NARRATIVE_RECOVERY_PROMPT}\n\nINPUT DATA:\n${JSON.stringify(prWithEvents)}`;
 
   try {
     const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      contents: prompt,
-      config: { responseMimeType: "application/JSON" },
+      contents: "hello",
+      config: {
+        responseMimeType: "application/json",
+      },
     });
-    const text = response.text as string;
-    return JSON.parse(text) as SummaryJSON;
+    const summary = JSON.parse(response.text!);
+    return summary;
   } catch (error) {
-    logger.error("Gemini AI Error:", error);
+    console.log("Gemini AI Error:", error);
     throw new Error("Failed to generate development narrative.");
   }
 }
+
+console.log(await makePRSummary("d" as any));
