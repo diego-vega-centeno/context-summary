71d06a2ee01004d515330d851f01e30f9f148885
diego vega <da-vce@hotmail.com>
2026-05-28 20:20:22 -0500

    feat: implement schemas and complete addPR

--- lib/actions/pr.ts
+++ lib/actions/pr.ts
@@ -1,9 +1,12 @@
 "use server";
-import { addPRData, fetchPRGithubIdentifiers, updatePRData } from "../data/prs";
+import { fetchPRGithubIdentifiers, updatePRData } from "../data/prs";
 import { revalidatePath } from "next/cache";
 import { makePRWithSummary } from "../data/transformers";
 import sql from "../db";
 import logger from "../logger";
+import { z } from "zod";
+import { redirect } from "next/navigation";
+import { ActionReturn } from "@/types";
 
 export async function syncPR(prId: string) {
   try {
@@ -32,27 +35,69 @@ export async function syncPR(prId: string) {
   }
 }
 
-export async function addPR(owner: string, repo: string, prNumber: number) {
+const PRURLSchema = z.object({
+  url: z
+    .string()
+    .regex(
+      /^https:\/\/github.com\/[^/]+\/[^/]+\/pull\/\d+$/,
+      "Please enter a valid GitHub Pull Request URL (e.g., https://github.com/owner/repo/pull/123)",
+    ),
+});
+
+const prParams = z.object({
+  owner: z.string(),
+  repo: z.string(),
+  prNumber: z.coerce.number(),
+});
+
+export async function addPR(
+  prevState: any,
+  formData: FormData,
+): Promise<ActionReturn | void> {
   try {
-    logger.info("Making PR with summary");
+    logger.info("Getting pr data from URL");
+    const formURL = formData.get("url");
+    const { url } = PRURLSchema.parse({ url: formURL });
+    const objectURL = new URL(url);
+    const segments = objectURL!.pathname.split("/").filter(Boolean);
+
+    const { owner, repo, prNumber } = prParams.parse({
+      owner: segments[0],
+      repo: segments[1],
+      prNumber: segments[3],
+    });
+
+    logger.info(`Adding PR: ${owner}/${repo} #${prNumber}`);
     const prWithSummary = await makePRWithSummary(owner, repo, prNumber);
 
-    logger.info("Adding PR with summary to database");
-    const prId = await addPRData(prWithSummary);
-
-    revalidatePath("/dashboard");
-    revalidatePath(`/stories/${prId}`);
-    return { success: true, data: prId };
+    logger.info("Adding PR to database");
+    // const prId = await addPRData(prWithSummary);
   } catch (error) {
-    logger.error("Add PR Error:", error);
+    logger.error("Add PR error:", error);
+
+    if (error instanceof z.ZodError) {
+      return {
+        success: false,
+        error: error.issues,
+      };
+    }
+
     return {
       success: false,
-      error:
-        error instanceof Error
-          ? error.message
-          : "An unexpected error occurred.",
+      error: [
+        {
+          code: "error",
+          message:
+            error instanceof Error
+              ? error.message
+              : "An unexpected error occurred.",
+        },
+      ],
     };
   }
+  revalidatePath("/dashboard");
+  revalidatePath(`/stories`);
+  redirect("/dashboard");
 }
 
 export async function syncActivePRs(userId: string) {
