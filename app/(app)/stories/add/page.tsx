edbd5092af94afb8682c44db7573babf253bb506
diego vega <da-vce@hotmail.com>
2026-05-28 20:20:42 -0500

    feat: create page for pr addition

---
+++ app/(app)/stories/add/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Button from "@/app/ui/Button";
+import { addPR } from "@/lib/actions/pr";
+import { ActionReturn } from "@/types";
+import { useActionState } from "react";
+
+export default function AddPRPage() {
+  const [state, formAction, isPending] = useActionState(addPR, null);
+  return (
+    <div className="flex justify-center px-40 p-6">
+      <form action={formAction} className="w-full" aria-live="polite">
+        <div className="space-y-3">
+          <div>
+            <label htmlFor="url">Pull request URL</label>
+          </div>
+          <input
+            id="url"
+            name="url"
+            type="text"
+            placeholder="Enter url ..."
+            className="w-full bg-sidebar-background p-2 rounded-md text-sm"
+          />
+          {state?.error &&
+            state.error.map((err: any) => (
+              <p key={err.code} className="text-red-500 text-sm">
+                {err.message}
+              </p>
+            ))}
+          <Button type="submit" disabled={isPending}>
+            {isPending ? "Analyzing Narrative..." : "Add PR"}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
