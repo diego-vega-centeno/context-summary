2f71d42389fec7146d3e94f1f0a3806c1df108b3
diego vega <da-vce@hotmail.com>
2026-06-15 15:46:05 -0500

    feat: redirect to lading page

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -17,17 +17,17 @@ export default function AppLayout({
           </div>
           <div className="flex flex-col gap-2 border-t-2 border-highlight p-2">
             <ThemeToggle showLabels className="w-full px-1" />
-            <Button icon={Settings} variant={"withIcon"} className="w-full">
+            <Button href="/" icon={Settings} variant={"withIcon"} className="w-full">
               Settings
             </Button>
-              <Button
-                type="submit"
-                variant={"withIcon"}
-                icon={LogOut}
-                className="w-full justify-start text-warning font-bold"
-              >
-                Sign out
-              </Button>
+            <Button
+              href="/"
+              variant={"withIcon"}
+              icon={LogOut}
+              className="w-full justify-start text-warning font-bold"
+            >
+              Sign out
+            </Button>
           </div>
         </aside>
       </div>
