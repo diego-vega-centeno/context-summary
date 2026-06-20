62b166269a1a6808e278643ce44cd4368b61b7d6
diego vega <da-vce@hotmail.com>
2026-06-20 18:55:35 -0500

    feat: use new supabase database

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,12 @@
 import postgres from "postgres";
 
 const connectionString =
+  process.env.SUPABASE_POSTGRE_DIRECT_URI! ||
   process.env.POSTGRES_URL! ||
   "postgresql://postgres:elefant4@localhost:5432/summary-context";
-const sql = postgres(connectionString);
+const sql = postgres(connectionString, {
+  ssl: "require",
+  prepare: false,
+});
 
 export default sql;
