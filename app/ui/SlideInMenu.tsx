8c0cd5b0ab75c8da68c1ea97c1628939200c9a03
diego vega <da-vce@hotmail.com>
2026-06-22 14:38:36 -0500

    feat: add slide in and fade animation

--- app/ui/SlideInMenu.tsx
+++ app/ui/SlideInMenu.tsx
@@ -2,9 +2,11 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X, Menu } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function SlideMenu({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+
   return (
     <Dialog.Root>
       <div className="flex items-center justify-between p-1 px-3 bg-sidebar-foreground text-white">
@@ -19,12 +21,10 @@ export default function SlideMenu({ children }: { children: React.ReactNode }) {
         <div>{pathname.replace("/", "")}</div>
       </div>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 z-40 bg-black/45 data-[state=open]:animate-overlayShow" />
+        <Dialog.Overlay className="fixed inset-0 z-40 bg-black/45 animate-overlayShow" />
         <Dialog.Content
-          className="fixed left-0 top-0 z-50 h-full w-72 max-w-[80vw] bg-white
-                     shadow-lg outline-none flex flex-col
-                     data-[state=open]:animate-slideInLeft
-                     data-[state=closed]:animate-slideOutLeft"
+          className="fixed left-0 top-0 z-50 h-full w-72 max-w-[80vw] 
+            shadow-lg outline-none flex flex-col animate-slideInLeft"
         >
           <div className="flex items-center justify-between px-2 py-1 border-b bg-sidebar-background text-sm">
             <Dialog.Title>Summary Context</Dialog.Title>
