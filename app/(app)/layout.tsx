ed9211ed02d471cfa8c1c411e25a24ed0bccdd52
diego vega <da-vce@hotmail.com>
2026-06-21 19:43:32 -0500

    feat: implement slide in menu for sm screens

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import SideNav from "@/app/ui/SideNav";
+import SlideMenu from "../ui/SlideInMenu";
 
 export default function AppLayout({
   children,
@@ -8,11 +9,16 @@ export default function AppLayout({
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
       <div className="md:w-64 max-h-screen">
-        <SideNav />
+        <div className="md:hidden">
+          <SlideMenu>
+            <SideNav />
+          </SlideMenu>
+        </div>
+        <div className="hidden md:block h-full">
+          <SideNav />
+        </div>
       </div>
-      <div className="flex-1 max-h-screen overflow-auto">
-        {children}
-      </div>
+      <div className="flex-1 max-h-screen overflow-auto">{children}</div>
     </div>
   );
 }
