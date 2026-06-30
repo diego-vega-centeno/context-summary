b1649aebcacc18a3c10746ded6cf8fbac8eb6ebf
diego vega <da-vce@hotmail.com>
2026-06-30 17:06:41 -0500

    feat: remove postgre local uri

--- lib/db.ts
+++ lib/db.ts
@@ -2,8 +2,7 @@ import postgres from "postgres";
 
 const connectionString =
   process.env.SUPABASE_POSTGRE_DIRECT_URI! ||
-  process.env.POSTGRES_URL! ||
-  "postgresql://postgres:elefant4@localhost:5432/summary-context";
+  process.env.POSTGRES_URL!
 const sql = postgres(connectionString, {
   ssl: "require",
   prepare: false,
