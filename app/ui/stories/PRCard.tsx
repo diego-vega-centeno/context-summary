c6ef5e72c84133bc56ee1e4d679a270769cd8a95
diego vega <da-vce@hotmail.com>
2026-07-09 20:35:16 -0500

    feat: make PRCards be the same height

--- app/ui/stories/PRCard.tsx
+++ app/ui/stories/PRCard.tsx
@@ -13,46 +13,52 @@ interface PRCardProps {
 export default function PRCard({ pr, refreshingPR, refreshPR }: PRCardProps) {
   const IconComponent = statusConfig[pr.status].icon;
   return (
-    <Link href={`stories/${pr.id}`}>
+    <Link href={`stories/${pr.id}`} className="h-full">
       <div
         key={pr.id}
-        className="text-sm border-1 border-border rounded-lg p-4 hover:bg-hover hover:cursor-default"
+        className="flex flex-col justify-between text-sm border-1 border-border rounded-lg p-4 pb-2 hover:bg-hover hover:cursor-default h-full"
       >
-        <div className="flex justify-between">
-          <div
-            className={`inline-flex items-center border-1 rounded-xl ${statusConfig[pr.status].color} px-2 mb-2 text-sm`}
-          >
-            <IconComponent className="inline-block w-6 pr-2" />
-            <span>{pr.status}</span>
-          </div>
-          <div>
-            <button
-              aria-label={`Refresh PR ${pr.external_id}`}
-              disabled={refreshingPR === pr.id}
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                refreshPR(pr.id);
-              }}
-              className={`hover:bg-highlight rounded-full p-1.5 ${refreshingPR === pr.id ? "opacity-50 cursor-not-allowed" : ""}`}
-            >
-              <RefreshCw
-                className={`w-4 h-4 ${refreshingPR === pr.id ? "animate-spin" : ""}`}
-              />
-            </button>
-          </div>
-        </div>
-        <div className="font-medium text-foreground">{pr.title}</div>
         <div className="">
-          <div className="py-2 text-muted-foreground">{`${pr.owner}/${pr.container} \u00B7 #${pr.external_id} \u00B7 ${pr.author}`}</div>
+          <div className="flex justify-between">
+            <div
+              className={`inline-flex items-center border-1 rounded-xl ${statusConfig[pr.status].color} px-2 mb-2 text-sm`}
+            >
+              <IconComponent className="inline-block w-6 pr-2" />
+              <span>{pr.status}</span>
+            </div>
+            <div>
+              <button
+                aria-label={`Refresh PR ${pr.external_id}`}
+                disabled={refreshingPR === pr.id}
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  refreshPR(pr.id);
+                }}
+                className={`hover:bg-highlight rounded-full p-1.5 ${refreshingPR === pr.id ? "opacity-50 cursor-not-allowed" : ""}`}
+              >
+                <RefreshCw
+                  className={`w-4 h-4 ${refreshingPR === pr.id ? "animate-spin" : ""}`}
+                />
+              </button>
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="font-medium text-foreground">{pr.title}</div>
+            <div className="">
+              <div className="py-2 text-muted-foreground">{`${pr.owner}/${pr.container} \u00B7 #${pr.external_id} \u00B7 ${pr.author}`}</div>
+            </div>
+            <div className="text-foreground py-2">{pr.current_state}</div>
+          </div>
         </div>
-        <div className="text-foreground py-2">
-          {pr.current_state}
-        </div>
-        <hr className="border-border" />
-        <div className="flex items-center pt-2 text-xs text-muted-foreground">
-          <Clock className="inline-block mr-2 w-4" />
-          <span>Last activity {formatRelativeDate(pr.last_activity_at.toString())}</span>
+        <div>
+          <hr className="border-border" />
+          <div className="flex items-center pt-2 text-xs text-muted-foreground">
+            <Clock className="inline-block mr-2 w-4" />
+            <span>
+              Last activity {formatRelativeDate(pr.last_activity_at.toString())}
+            </span>
+          </div>
         </div>
       </div>
     </Link>
