2bad5264d26fd5699289663e2899942f14f28dba
diego vega <da-vce@hotmail.com>
2026-07-09 21:14:19 -0500

    feat: add author to PRMiniCard

--- app/ui/dashboard/PRMiniCard.tsx
+++ app/ui/dashboard/PRMiniCard.tsx
@@ -5,14 +5,18 @@ export default function PRMiniCard({ pr }: { pr: WorkItemDashboardType }) {
   return (
     <Link
       href={`stories/${pr.id}`}
-      className="text-sm border border-border rounded-lg p-3 hover:bg-hover hover:cursor-pointer"
+      className="flex flex-col space-y-2 text-sm border border-border rounded-lg p-3 hover:bg-hover hover:cursor-pointer"
     >
       <div className="font-medium text-foreground">{pr.title}</div>
-      <div className="text-muted-foreground pb-2">
-        <div className="pt-2">{`#${pr.external_id} - ${pr.container}`}</div>
+      <hr className="border-border" />
+      <div>
+        <div className="text-muted-foreground text-white">{pr.author}</div>
+        <div className="text-muted-foreground">
+          {`#${pr.external_id} - ${pr.container}`}
+        </div>
       </div>
       <hr className="border-border" />
-      <div className="pt-2">{pr.current_state}</div>
+      <div>{pr.current_state}</div>
     </Link>
   );
 }
