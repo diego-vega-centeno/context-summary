42e27ebbe1a0701d212e798b32b2889c88dc7aa8
diego vega <da-vce@hotmail.com>
2026-07-25 15:09:02 -0500

    feat: improve stylings

--- app/ui/SelectionCollapsible.tsx
+++ app/ui/SelectionCollapsible.tsx
@@ -19,14 +19,14 @@ export default function SelectionCollapsible({
       open={isOpen}
       onOpenChange={(open: boolean) => setCurrentOpen(open ? field : null)}
     >
-      <DropdownMenu.Trigger className="outline-none">
+      <DropdownMenu.Trigger className="outline-none text-sm">
         <div
           className={`
             flex justify-between items-center font-semibold px-2 py-1 border 
-            border-border rounded-lg hover:bg-hover
+            border-border rounded-lg hover:bg-hover hover:cursor-pointer
             ${isOpen ? "bg-hover text-white" : "hover:bg-hover hover:text-white text-muted-foreground"}`}
         >
-          <div>{field}</div>
+          <div className="pr-1">{field}</div>
           <div>
             {isOpen ? (
               <ChevronDown className="size-5" />
@@ -38,7 +38,9 @@ export default function SelectionCollapsible({
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content
-          className={`w-[200px] bg-sidebar-background overflow-hidden ${isOpen ? "animate-collapsible-down" : "animate-collapsible-up"}`}
+          className={`min-w-[200px] border border-border rounded-lg 
+            bg-sidebar-background overflow-hidden mt-2
+            `}
         >
           <div>{content}</div>
         </DropdownMenu.Content>
